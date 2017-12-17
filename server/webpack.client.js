e20d3a4cfed3199cef7ad537a8a2f2688ab5a237
Permadi Wibisono <mbapewe@gmail.com>
2017-12-17 19:54:59 +0700

    Add webpack.base.js for refactor webpack config

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const base = require('./webpack.base.js');
 
-module.exports = {
+const config = {
 
   // Tell webpack the root file of our
   // server application
@@ -11,23 +13,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
-  },
-
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(base,config);

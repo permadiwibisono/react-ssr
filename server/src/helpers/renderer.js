1cf2a34532b7f68a51428a523f310977349feab8
Permadi Wibisono <mbapewe@gmail.com>
2017-12-17 20:23:18 +0700

    Add renderer helpers

---
+++ server/src/helpers/renderer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import Home from '../client/components/Home';
+
+export default ()=>{    
+    const content = renderToString(<Home/>);
+    return `
+        <html>
+            <head>
+                <title>Example Server Side Rendering on React</title>
+            </head>
+            <body>
+                <div id="root">${content}</div>
+                <script src="bundle.js"></script>
+            </body>
+        </html>
+    `
+}

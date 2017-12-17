87d7f5d1ef502a2d481f9679d542f74ddfdcaf5c
Permadi Wibisono <mbapewe@gmail.com>
2017-12-17 16:59:46 +0700

    Adding src inside server folder

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Home = ()=>{
+    return (<div>I'm the Home page</div>)
+}
+export default Home;
---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/',(req,res)=>{
+    
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening port 3000');
+})

80604ab00043c6dec34802e21936b1d2127b4583
Permadi Wibisono <mbapewe@gmail.com>
2017-12-17 17:41:09 +0700

    Include react on express

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
 
 app.get('/',(req,res)=>{
-    
+    const content = renderToString(<Home/>);
+    res.send(content);
 })
 
 app.listen(3000, ()=>{

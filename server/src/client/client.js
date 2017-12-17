56362d4ce306f885c45487fa2a68fa5b4bca573c
Permadi Wibisono <mbapewe@gmail.com>
2017-12-18 02:45:45 +0700

    Add simple users redux

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import reducers from './reducers';
 
 import Routes from './Routes';
 
--- server/src/helpers/createStore.js
+++ server/src/helpers/createStore.js
@@ -1,5 +1,6 @@
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import reducers from '../client/reducers';
 
 export default ()=>(
     createStore(reducers,{},applyMiddleware(thunk))

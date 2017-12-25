21fcd57ca15d146c89a6b459b140113def45fded
Permadi Wibisono <mbapewe@gmail.com>
2017-12-26 02:31:12 +0700

    Proxy setup for Cookies Authentication

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,18 @@
 import 'babel-polyfill';
 import express from 'express';
+import proxy from 'express-http-proxy';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
 
 const app = express();
+app.use('/api',proxy('https://react-ssr-api.herokuapp.com',{
+    proxyReqOptDecorator(opts){
+        opts.header['x-forwarded-host']='http://localhost:3000';
+        return opts;
+    }
+}))
 app.use(express.static('public'));
 app.get('*',(req,res)=>{
     const store = createStore();

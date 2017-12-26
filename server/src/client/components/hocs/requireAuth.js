1eac772b7b45fedcefddc5332f86e47286fd69e5
Permadi Wibisono <mbapewe@gmail.com>
2017-12-27 00:52:28 +0700

    Add RequireAuth as high order components

---
+++ server/src/client/components/hocs/requireAuth.js
@@ -0,0 +1,20 @@
+import React, { PureComponent as Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+export default ChildComponent => {
+    class RequireAuth extends Component{
+        render(){
+            switch(this.props.auth){
+                case false:
+                    return <Redirect to="/"/>
+                case null:
+                    return <div>Loading...</div>
+                default:
+                    return <ChildComponent {...this.props}/>
+            }
+        }
+    }
+    const mapStateToProps = ({ auth }) => ({ auth })
+    return connect(mapStateToProps)(RequireAuth);
+}
